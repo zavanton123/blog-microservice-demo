8ca7c335a3d41aa388c5452d0cd352b5126e184a
zavanton123 <zavanton123@gmail.com>
2021-06-21 12:39:49 +0300

    post, comment and template client

--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
+
+// in-memory storage for posts
+const posts = {};
 
 app.get('/posts', (req, res) => {
-
+  res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-
+  // generate a random id
+  const id = randomBytes(4).toString('hex');
+  const {title} = req.body;
+  posts[id] = {id, title};
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
