e8b98bdfb25c0c726c6f9f509b0cf74e2f6f46dd
zavanton123 <zavanton123@gmail.com>
2021-06-21 17:44:36 +0300

    client updated to show moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 export default ({comments}) => {
   const renderedComments = Object.values(comments).map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+    if (comment.status === 'pending') {
+      content = 'This comment is waiting moderation';
+    }
+    if (comment.status === 'rejected') {
+      content = 'This comment has been rejected';
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
