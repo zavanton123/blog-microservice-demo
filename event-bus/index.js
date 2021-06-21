39d7a87a69fe5bf8609976babd35a53d4f87afa5
zavanton123 <zavanton123@gmail.com>
2021-06-21 13:58:15 +0300

    event bus created

---
+++ event-bus/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+  // send to posts
+  await axios.post('http://localhost:4000/events', event);
+  // send to comments
+  await axios.post('http://localhost:4001/events', event);
+  // send to query service
+  await axios.post('http://localhost:4002/events', event);
+  res.send({status: 'OK'});
+});
+
+app.listen(4005, () => {
+  console.log(`zavanton - listening on 4005`);
+});
